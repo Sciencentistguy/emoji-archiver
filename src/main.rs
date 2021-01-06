880245bd4d0f9fe2114373b8ad57f2d231996d3d
Jamie Quigley <jamie@quigley.xyz>
2021-01-06 12:15:40 +0000

    Setup proper logging, now downloads into a folder

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+use log::*;
 use std::env;
 use std::fs::File;
 use std::io;
@@ -14,6 +15,11 @@ const PATH_PREFIX: &str = "/dev/shm/";
 
 #[tokio::main]
 async fn main() {
+    // Set default log level to info unless otherwise specified.
+    if std::env::var("RUST_LOG").is_err() {
+        std::env::set_var("RUST_LOG", "emoji_archiver=info");
+    }
+    pretty_env_logger::init();
     let opts = Opt::from_args();
     let token = if opts.token.is_some() {
         opts.token.unwrap()
@@ -37,7 +43,7 @@ async fn main() {
     // Shards will automatically attempt to reconnect, and will perform
     // exponential backoff until it reconnects.
     if let Err(why) = client.start().await {
-        println!("Client error: {:?}", why);
+        error!("Client error: {:?}", why);
     }
 }
 
@@ -47,18 +53,35 @@ struct Handler;
 impl EventHandler for Handler {
     async fn message(&self, ctx: Context, msg: Message) {
         if msg.content == "!archive" {
+            let guild_name = msg.guild(&ctx.cache).await.unwrap().name;
+            info!(
+                "Archive started by {} in server '{}'",
+                msg.author.name, guild_name
+            );
             let server_id = msg.guild_id.unwrap();
             let emojis = server_id.emojis(&ctx.http).await.unwrap();
+            let download_subdirectory = format!(
+                "{}-{}",
+                guild_name.replace(" ", "-").to_lowercase(),
+                chrono::Utc::now().format("%Y-%m-%dT%H-%M-%S")
+            );
             for emoji in emojis {
-                println!("{:?}", emoji);
                 let url = emoji.url();
-                println!("{}", url);
                 let name = &emoji.name;
-                let ext = if emoji.animated { "gif" } else { "png" };
-                let path = format!("{}/{}.{}", PATH_PREFIX, name, ext);
+                let ext = &url[url
+                    .rfind(".")
+                    .expect("Emoji url does not have a file extension")..];
+                let path = format!("{}/{}/{}{}", PATH_PREFIX, download_subdirectory, name, ext);
+                info!(
+                    "Downloading emoji {} from url {} to location {}...",
+                    name, url, path
+                );
                 download_url(&url, &path).await.unwrap();
             }
-            msg.reply(&ctx, "Archive complete.").await.expect("Error sending message");
+            msg.reply(&ctx, "Archive complete.")
+                .await
+                .expect("Error sending message");
+            info!("Archive complete.");
         }
     }
 
@@ -69,7 +92,7 @@ impl EventHandler for Handler {
     //
     // In this case, just print what the current user's username is.
     async fn ready(&self, _: Context, ready: Ready) {
-        println!("{} is connected!", ready.user.name);
+        info!("{} is connected!", ready.user.name);
     }
 }
 
@@ -78,21 +101,28 @@ async fn download_url<P: AsRef<Path>>(
     destination: P,
 ) -> Result<(), Box<dyn std::error::Error>> {
     let response = reqwest::get(url).await.unwrap();
-
     let mut dest = {
+        let destdir = destination
+            .as_ref()
+            .parent()
+            .expect("Destination path did not have a parent");
+        if !destdir.is_dir() {
+            std::fs::create_dir(destdir)?;
+        };
         let fname = response
             .url()
             .path_segments()
             .and_then(|segments| segments.last())
             .and_then(|name| if name.is_empty() { None } else { Some(name) })
             .unwrap_or("<unnamed>");
-        println!("file to download: '{}'", fname);
-        println!("Will be stored in '{:?}'", destination.as_ref());
+        trace!("file to download: '{}'", fname);
+        trace!("Will be stored in '{:?}'", destination.as_ref());
         File::create(destination.as_ref())?
     };
 
     let mut content = io::Cursor::new(response.bytes().await?);
     std::io::copy(&mut content, &mut dest)?;
+    info!("Download complete");
 
     Ok(())
 }
